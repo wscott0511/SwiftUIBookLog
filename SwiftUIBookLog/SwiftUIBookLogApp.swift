25da226d8207af5804865031964405fd61446801
Ryan Scott <rscott.sites@gmail.com>
2022-06-07 13:42:11 -0700

    Initial Commit

---
+++ SwiftUIBookLog/SwiftUIBookLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBookLogApp.swift
+//  SwiftUIBookLog
+//
+//  Created by Ryan Scott on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBookLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
